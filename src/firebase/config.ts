2423cc8883cfed8abc0b5498253ff0b640e6f967
fernando sena <fernandopadre24@gmail.com>
2025-10-23 01:29:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6055309321-efeaf",
+  "appId": "1:1006792365943:web:39ac05c0d034c59d7f3372",
+  "apiKey": "AIzaSyB-4krBxmybKWWcbHv1d88RKZJILTKY8Yw",
+  "authDomain": "studio-6055309321-efeaf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1006792365943"
+};
